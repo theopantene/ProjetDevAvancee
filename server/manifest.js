c4988e88e877de125bd7f415cdbb7c49815a8950
Devin Ivy <devin@bigroomstudios.com>
2021-04-11 23:24:33 -0400

    initial commit

--- server/manifest.js
+++ server/manifest.js
@@ -34,9 +34,6 @@ module.exports = new Confidence.Store({
                 options: {}
             },
             {
-                plugin: './plugins/swagger'
-            },
-            {
                 plugin: {
                     $filter: 'NODE_ENV',
                     $default: '@hapipal/hpal-debug',
