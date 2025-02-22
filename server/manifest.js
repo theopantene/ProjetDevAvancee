7f5e7a8f95d185b84624053e3c04faf967bde836
Théo Pantene <theo.pantene@etu.unilim.fr>
2025-02-22 17:33:19 +0100

    initial commit

--- server/manifest.js
+++ server/manifest.js
@@ -58,10 +58,26 @@ module.exports = new Confidence.Store({
                 }
             },
             {
-                plugin: {
+                plugin: '@hapipal/schwifty',
+                options: {
                     $filter: 'NODE_ENV',
-                    $default: '@hapipal/hpal-debug',
-                    production: Toys.noop
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'mysql',
+                            connection: {
+                                host: process.env.DB_HOST || '0.0.0.0',
+                                user: process.env.DB_USER || 'root',
+                                password: process.env.DB_PASSWORD || 'hapi',
+                                database: process.env.DB_DATABASE || 'user',
+                                port: process.env.DB_PORT || 3306
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
                 }
             }
         ]
